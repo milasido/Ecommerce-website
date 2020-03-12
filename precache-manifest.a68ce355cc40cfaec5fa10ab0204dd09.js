460ea619f119b307b2d0be08e85e1ae99d2b6a75
DEAN <wolnguyen98@gmail.com>
2020-03-12 02:37:50 -0500

    Updates

---
+++ precache-manifest.a68ce355cc40cfaec5fa10ab0204dd09.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0226b966e98b407d8119f2aa2954f1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15e9c4b84d2c6e59f6a9",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "479e066a91097284412d",
+    "url": "/static/css/main.5cfa5038.chunk.css"
+  },
+  {
+    "revision": "15e9c4b84d2c6e59f6a9",
+    "url": "/static/js/2.18c4b75c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.18c4b75c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "479e066a91097284412d",
+    "url": "/static/js/main.45a22394.chunk.js"
+  },
+  {
+    "revision": "791531edb4a5d2c49beb",
+    "url": "/static/js/runtime-main.88452ce1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
