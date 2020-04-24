c127d2327a2951ffc347fc18f9f6051a5064daef
kimnguyen137 <kimnguyen137@gmail.com>
2020-04-24 15:39:18 -0500

    Confirm order - Thank you

--- ClientApp/src/components/ThankYou.js
+++ ClientApp/src/components/ThankYou.js
@@ -6,12 +6,27 @@ export class ThankYou extends Component {
     render() {
         return (
             <div>
-                Thank you for your purchase!
-                To see you purchase please go to your order history in user account!
-                <Link to='/'>
-                    <button>Home page</button>
-                </Link>
+                        <div className="container" style={{marginTop: '5%'}}>
+                <div className="row">
+                <div className="jumbotron" style={{boxShadow: '2px 2px 4px #000000'}}>
+                    <h1 className="text-center">YOUR ORDER HAS BEEN RECEIVED</h1>
+                    <h3 className="text-center">Thank you for your payment, it’s processing</h3>
+                    <p className="text-center">Your order # is: KNN20192020</p>
+                    <p className="text-center">You can view your order history in your account information. </p>
+                    <p className="text-center">If you have any question please contact our service on main website.</p>
+                    <center><div className="btn-group" style={{marginTop: '50px'}}>
+                    <Link to='/'>
+                        <button className="btn btn-lg btn-warning">CONTINUE</button>
+                    </Link>
+                        
+                    </div></center>
+                </div>
+                </div>
             </div>
+
+                
+            </div>
+            
         )
     }
 }
